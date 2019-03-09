6c7d8381a94cbdd195685df7811011320096dc5a
joaocarlospereira <jcp2105@gmail.com>
2019-03-09 14:15:27 -0300

    Aula 09/03

--- LaPelicula/src/screen/ListaFilmesScreen.js
+++ LaPelicula/src/screen/ListaFilmesScreen.js
@@ -1,14 +1,80 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableHighlight, ImageBackground } from 'react-native';
+import { openDatabase } from 'react-native-sqlite-storage';
+import { FlatList } from 'react-native-gesture-handler';
+var db = openDatabase({ name: 'lapelicula.db' });
+
+
+//Component de Filmes
+class Filmes extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <TouchableHighlight onPress={() => alert("Filme: " + this.props.data.descricao)} underlayColor="blue" >
+
+                    <ImageBackground resizeMode="cover" source={{ uri: this.props.data.imagem }} style={{ height: 150 }}>
+                        <View style={{
+                            flex: 1,
+                            alignItems: 'flex-start',
+                            justifyContent: 'flex-end',
+                            paddingLeft: 10,
+                            paddingBottom: 10
+                        }}>
+                            <Text style={{ fontSize: 23, color: '#FFFFFF', fontWeight: 'bold' }}>{this.props.data.descricao}</Text>
+                        </View>
+                    </ImageBackground>
+                </TouchableHighlight>
+            </View>
+        );
+    }
+}
 
 
 export default class ListaFilmesScreen extends Component {
 
+      // configurando opções de navegação
+      static navigationOptions = ({navigation}) => ({
+        /*title: 'Página Principal'*/
+        tabBarLabel: 'Home',
+        tabBarIcon: ({focused, tintColor}) => {
+          if (focused) {
+            return (
+              <Image source={require('../img/home_ativo.png')}
+              style={{width: 26, height: 26}}/>
+            );
+          } else {
+            return (
+              <Image source={require('../img/home_inativo.png')}
+              style={{width: 26, height: 26}}/>
+            );            
+          }
+        }
+    });
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      filmes: []
+    };
+
+    // buscar dados na base
+    db.transaction(tx => {
+      tx.executeSql('SELECT * from filme ORDER BY descricao', [], (tx, res) => {
+        //Tratar o resultado da consulta.
+        var temp = [];
+        for (let i = 0; i < res.rows.length; i++) {
+          temp.push(res.rows.item(i));
+        }
+        //seta os filmes para exibir no flatlist
+        this.setState({ filmes: temp });
+      } ) 
+    })
+  }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Lista de filmes</Text>
+        <FlatList data={this.state.filmes} keyExtractor={item => item.codigo.toString()} renderItem={({item}) => <Filmes data={item}/>}></FlatList>
       </View>
     );
   }
@@ -17,7 +83,6 @@ export default class ListaFilmesScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    backgroundColor: 'white'
   }
 });
